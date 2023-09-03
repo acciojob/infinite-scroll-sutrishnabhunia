fbc90d43e1a53d31671e142a1ba4bfc1bb9a8e9f
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-09-03 20:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 //your code here!
+//your code here!
+ 
+        const list = document.getElementById('infi-list');
+    const initialItemsCount = 10;
+    let currentItemsCount = initialItemsCount;
+     
+    // Function to add list items
+    function addListItems(count) {
+      for (let i = 1; i <= count; i++) {
+        const li = document.createElement('li');
+        li.textContent = `List Item ${currentItemsCount + i}`;
+        list.appendChild(li);
+      }
+      currentItemsCount += count;
+    }
+     
+    // Add initial list items
+    addListItems(initialItemsCount);
+     
+    // Function to check if user reached the end of the list
+    function checkScroll() {
+      const scrollTop = list.scrollTop;
+      const scrollHeight = list.scrollHeight;
+      const clientHeight = list.clientHeight;
+     
+      if (scrollTop + clientHeight >= scrollHeight) {
+        // Add 2 more list items when reaching the end
+        addListItems(2);
+      }
+    }
+     
+    // Listen for scroll events
+    list.addEventListener('scroll', checkScroll);
+
 
